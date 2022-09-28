50dfbc94d99f47a547baf14cdb114062b27d8cd9
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2022-09-27 21:57:23 -0400

    Added new dependencies needed for first REST controller

---
+++ api/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.4"
+	id("io.spring.dependency-management") version "1.0.14.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "com.kwizzlr.journaler"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework:spring-context:5.3.23")
+	implementation("org.springframework:spring-aop:5.3.23")
+	implementation("org.springframework.boot:spring-boot-starter-web:2.7.3")
+	implementation("org.springframework.boot:spring-boot-starter-actuator:2.7.3")
+	implementation("org.springframework:spring-webmvc:5.3.23")
+	implementation("org.springframework:spring-web:5.3.23")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
